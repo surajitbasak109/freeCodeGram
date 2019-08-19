ce51f995ccf6a5eba07875b2d99931e5de289455
Surajit Basa <surajitbasak109@gmail.com>
2019-08-19 23:58:51 +0530

    first commit

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,9 @@
+@component('mail::message')
+# Welcome To freeCodeGram
+
+This is a community of fellow developers and we love that you have joined us.
+
+
+All the best, <br>
+Surajit
+@endcomponent
